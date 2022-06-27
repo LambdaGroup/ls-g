31545b2e62d86ae0c803d9b81de01c9d558adf77
Mulling <lucas-mulling@protonmail.com>
2022-06-27 20:42:45 -0300

    feat(scripts): reduce bloat

--- lfs/scripts/lfsqemu.sh
+++ lfs/scripts/lfsqemu.sh
@@ -2,7 +2,87 @@
 
 mkdir -p "$HOME/.config/lfs/"
 
-source "$(pwd)/utils/spinner.sh"
+# Bloat courtesy of Mr. Daniel
+
+# Spinner Author: Tasos Latsas
+#
+# Display an awesome 'spinner' while running your long shell commands
+#
+# Do *NOT* call _spinner function directly.
+# Use {start,stop}_spinner wrapper functions
+
+function _spinner() {
+    # $1 start/stop
+    #
+    # on start: $2 display message
+    # on stop : $2 process exit status
+    #           $3 spinner function pid (supplied from stop_spinner)
+
+    local on_success="DONE"
+    local on_fail="FAIL"
+    local white="\e[1;37m"
+    local green="\e[1;32m"
+    local red="\e[1;31m"
+    local nc="\e[0m"
+
+    case $1 in
+        start)
+            # calculate the column where spinner and status msg will be displayed
+            let column=$(tput cols)-${#2}-8
+            # display message and position the cursor in $column column
+            echo -ne ${2}
+            printf "%${column}s"
+
+            # start spinner
+            i=1
+            sp='\|/-'
+            delay=${SPINNER_DELAY:-0.15}
+
+            while :
+            do
+                printf "\b${sp:i++%${#sp}:1}"
+                sleep $delay
+            done
+            ;;
+        stop)
+            if [[ -z ${3} ]]; then
+                echo "spinner is not running.."
+                exit 1
+            fi
+
+            kill $3 > /dev/null 2>&1
+
+            # inform the user uppon success or failure
+            echo -en "\b["
+            if [[ $2 -eq 0 ]]; then
+                echo -en "${green}${on_success}${nc}"
+            else
+                echo -en "${red}${on_fail}${nc}"
+            fi
+            echo -e "]"
+            ;;
+        *)
+            echo "invalid argument, try {start/stop}"
+            exit 1
+            ;;
+    esac
+}
+
+function start_spinner {
+    # $1 : msg to display
+    _spinner "start" "${1}" &
+    # set global spinner pid
+    _sp_pid=$!
+    disown
+}
+
+function stop_spinner {
+    # $1 : command exit status
+    _spinner "stop" $1 $_sp_pid
+    unset _sp_pid
+}
+
+# end Bloat
 
 # aux funcs
 usage() { echo "Usage: $0 [ -d DISK_IMG_PATH ] [-s DISKS_IMG_SIZE ] [ -r RAM ] [ -p ISO_PATH ] [ -l <ssh | live> ]" 1>&2; exit 1; }
@@ -63,19 +143,14 @@ while getopts :d:s:r:p:hl: o; do
 done
 shift $((OPTIND-1))
 
-start_spinner "Using disk image: $DISK_IMG_PATH"
-sleep 0.1
-stop_spinner $?
-
 # if the disk image not exits, create one
 if ! [ -f "$DISK_IMG_PATH" ]; then
-    # echo "Creating virtual disk image"
-    start_spinner "Creating virtual disk image"
+    start_spinner "Creating virtual disk image on: $DISK_IMG_PATH"
     sleep 0.1
     qemu-img create -f raw "$DISK_IMG_PATH" "$DISK_IMG_SIZE">/dev/null
     stop_spinner $?
 else
-    start_spinner "Virtual disk image found"
+    start_spinner "Using disk image: $DISK_IMG_PATH"
     sleep 0.1
     stop_spinner $?
 fi
@@ -83,7 +158,7 @@ fi
 PROCS="$(($(nproc) / 2))"
 
 start_spinner "Running system. Wait to qemu to launch"
-sleep 0.1
+# sleep 0.1
 case "$BOOT_FROM" in
     ssh )
         qemu-system-x86_64 \
@@ -97,7 +172,7 @@ case "$BOOT_FROM" in
             -net nic \
             -net user,hostfwd=tcp:127.0.0.1:2222-:22 \
             -smp "$PROCS",sockets=1,cores="$PROCS" >/var/tmp/arch-linux-vm.log 2>&1 & \
-        ;;
+       ;;
 
     live )
         qemu-system-x86_64 \
@@ -111,7 +186,11 @@ case "$BOOT_FROM" in
             -cdrom "$ISO_PATH" >/var/tmp/arch-linux-vm.log 2>&1 & \
         ;;
     * )
+        stop_spinner $?
         usage
         ;;
 esac
-stop_spinner $?
+R="$?"
+# wait for the VM to boot
+sleep 10
+stop_spinner $R
